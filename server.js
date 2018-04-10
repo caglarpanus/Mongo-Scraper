30c46e1494729831f3b70e66d2e7c111440ca71d
caglarpanus <panuscaglar@gmail.com>
2018-04-10 09:59:35 -0400

    models and view is done working on server

--- server.js
+++ server.js
@@ -1,70 +1,46 @@
-//Dependencies 
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
+var path = require("path");
 
-// Our scraping tools
+// Requiring Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+// Scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
 
-// Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-//Set up the port
-var PORT = process.env.PORT || 8080;
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+//Define port
+var port = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
-// Use body parser
-app.use(bodyParser.urlencoded({extended: false}));
-// Make public a static directory
-app.use(express.static(process.cwd() + "/public"));
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    modalDir: path.join(__dirname, "/views/layouts/modal")
+}));
+app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
 var databaseUri = "mongodb://localhost/mongoosearticles";
-
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-  } else {
-    mongoose.connect(databaseUri);
-  }
-  
-  var db = mongoose.connection;
-
-//set engine and default for handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-//Scraping articles from New York Times
-var scrape = function(cb) {
-
-    var articlesArr = [];
-  
-    request("https://www.nytimes.com/", function(error, response, html) {
-  
-        var $ = cheerio.load(html);
-  
-  
-        $("h2.story-heading").each(function(i, element) {
-  
-            var result = {};
-  
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
-  
-            if (result.title !== "" && result.link !== "") {
-                articlesArr.push(result);
-            }
-        });
-        cb(articlesArr);
-    });
-  
-  };
-  
-
-//setup listener
-app.listen(port, function() {
-    console.log("App running on port " + PORT);
-  });
+//mongoose.connect("mongodb://localhost/mongoscraper");
+var db = mongoose.connection;
