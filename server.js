f994057ba123d4a0a1b2dd9b44ded18fea8152b4
caglarpanus <panuscaglar@gmail.com>
2018-04-11 20:18:35 -0400

    working on app.js for UI handling

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var cheerio = require("cheerio");
 mongoose.Promise = Promise;
 
 //Define port
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
@@ -42,10 +42,10 @@ app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
 var databaseUri = "mongodb://localhost/mongoosearticles";
-//mongoose.connect("mongodb://localhost/mongoscraper");
+
 var db = mongoose.connection;
 
-// A GET request to scrape the echojs website
+// A GET request to scrape the website
 
 app.get("/scrape", function(req,res){
     //We first must grab the body of the html
@@ -136,7 +136,6 @@ app.get("/articles/:id", function(req,res){
 });
 
 //Save an article
-
 app.post("/articles/saved/:id", function(req,res){
     //we must use id to find and update its saved boolean
     Article.findOneAndUpdate({"_id":req.params.id}, {"saved":true})
@@ -151,3 +150,72 @@ app.post("/articles/saved/:id", function(req,res){
         }
     });
 });
+
+//Delete an article
+app.post("/articles/delete/:id", function(req,res){
+     //we must use id to find and update its saved boolean
+     Article.findByIdAndUpdate({"_id":req.params.id},{"saved":false, "notes":[]})
+     .then(function(err,data){
+         if(err){
+             console.log(err);
+         }
+         else{
+             res.send(data);
+         }
+     })
+})
+
+//create or add a new note 
+app.post("/notes/save/:id", function(req,res){
+    //create a new not and this must past through req.body
+    var newNote = new Note({
+        body:req.body.text,
+        article:req.params.id
+    });
+    console.log(req.body);
+    //saving new note to database
+    newNote.save(function(err,note){
+        if(err){
+            console.log(err);
+        }
+        else{
+            Article.findOneAndUpdate({"_id":req.params.id}, {$push:{"notes":note}})
+            .then(function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.send(note);
+                }
+            });
+        }
+    });
+});
+
+// Delete a note
+app.delete("/notes/delete/:note_id/:article_id", function(req,res){
+    //use note ID to find and delete it
+    Note.findOneAndRemove({"_id":req.params.note_id}, function(err){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }
+        else{
+            Article.findOneAndUpdate({"_id":req.params.article_id},{$pull:{"notes":req.params.note_id}})
+            .then(function(err){
+                if(err){
+                    console.log(err);
+                    res.send(err);
+                }
+                else{
+                    res.send("Note Deleted")
+                }
+            })
+        }
+    })
+})
+
+// Listen on port
+app.listen(port, function() {
+    console.log("App running on port " + PORT);
+  });
